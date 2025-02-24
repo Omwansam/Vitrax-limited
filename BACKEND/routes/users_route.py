9526df7b36f52aca3e70bdcd864ea70e0b2f3e88
quincy mwangi <mwangiquincy77@gmail.com>
2025-02-24 18:51:28 +0300

    changes

--- BACKEND/routes/users_route.py
+++ BACKEND/routes/users_route.py
@@ -1,4 +1,4 @@
-'''from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from extensions import db
 from models import User
@@ -39,6 +39,6 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"message": "User created successfully"}), 201'''
+    return jsonify({"message": "User created successfully"}), 201
 
 
